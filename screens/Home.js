e1a39b35f41fc84771830d7f606068d6aeb34634
bombh <fabrice@bxxl.be>
2023-11-14 00:37:55 +0100

    Home User & Search Bar desing

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,10 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+
+import {
+   ChevronDownIcon, UserIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon
+} from 'react-native-heroicons/outline'
 
 const Home = () => {
    const navigation = useNavigation()
@@ -12,8 +16,41 @@ const Home = () => {
    }, [])
 
    return (
-      <SafeAreaView className="bg-red-400">
-         <Text className="text-center font-black p-4">Home</Text>
+      <SafeAreaView className="bg-white">
+         {/* Header */}
+         <View className="flex-row items-center space-x-2 p-3">
+            <Image
+               source={{
+                  uri: 'https://barnies.fr/wp-content/uploads/2016/03/Picasso-square.jpg'
+               }}
+               resizeMode='contain'
+               className="w-12 h-12 bg-gray-300 p-4 rounded-full"
+            />
+
+            <View className="flex-1">
+               <Text className="font-bold text-gray-400 text-xs">Deliver now !</Text>
+               <Text className="font-bold text-xl">
+                  Current location
+                  <ChevronDownIcon size={20} color={"#00CCBB"} />
+               </Text>
+            </View>
+
+            <UserIcon size={35} color={"#00CCBB"} />
+         </View>
+
+         {/* Search */}
+         <View className="flex-row items-center space-x-2 pb-3 mx-3">
+            <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+               <MagnifyingGlassIcon color="gray" size="20" />
+               <TextInput
+                  placeholder='Restaurants & Cuisines'
+                  keyboardType='default'
+               />
+            </View>
+
+            <AdjustmentsVerticalIcon size={25} color={"#00CCBB"} />
+         </View>
+
       </SafeAreaView>
 )
 }
